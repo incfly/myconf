42da1f14ca1fd7404123f22554be3209b56cd138
Jianfei Hu <jianfeih@google.com>
2018-11-18 10:41:30 +0000

    add kubectl install

--- setup.sh
+++ setup.sh
@@ -16,6 +16,14 @@ function golang_setup() {
   rm -rf tmp
 }
 
+function kubectl_setup() {
+  sudo apt-get update && sudo apt-get install -y apt-transport-https
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  sudo apt-get update
+  sudo apt-get install -y kubectl
+}
+
 function myconf_setup(){
     # install oh-my-zsh
     echo "installing oh-my-zsh..."
