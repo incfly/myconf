432d29eeb771f3defbac6ac0cc621dff1d4327bb
Jianfei Hu <jianfeih@google.com>
2019-07-08 21:33:55 +0000

    not load emsdk...

--- emsdk/config.sh
+++ emsdk/config.sh
@@ -1,1 +1,4 @@
-source $HOME/workspace/github.com/emscripten-core/emsdk/emsdk_env.sh 2>&1 >/dev/null
+# emsdk cause clang++ pointed to its own pre-installed version... very troublesome.
+loademsdk() {
+  source $HOME/workspace/github.com/emscripten-core/emsdk/emsdk_env.sh 2>&1 >/dev/null
+}
