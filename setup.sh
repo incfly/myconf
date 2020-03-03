f1cd75942690dbc261be577a4870799259fd26c2
Jianfei Hu <jianfeih@google.com>
2020-03-02 16:52:35 -0800

    ssh config

--- setup.sh
+++ setup.sh
@@ -53,8 +53,9 @@ function myconf_setup(){
     # install oh-my-zsh
     echo "installing oh-my-zsh..."
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    echo "copy settings(vim, tmux, zsh, dircolors)..."
+    echo "copy settings(vim, tmux, zsh, dircolors, ssh)..."
     cp vim/.vimrc ~/
+    mkdir -p $HOME/.ssh/ && cp ssh/config ~/.ssh/
     cp -r vim/.vim ~/
     cp tmux/.tmux.conf ~/
     ln -s git/.gitconfig $HOME/.gitconfig
