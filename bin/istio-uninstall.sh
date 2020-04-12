b0690ca1d7e1761647c7caf63dde9db5a956b225
Jianfei Hu <jianfeih@google.com>
2020-04-12 21:13:19 +0000

    istio-cli tool

---
+++ bin/istio-uninstall.sh
@@ -0,0 +1,6 @@
+krm dr --all --all-namespaces
+krm policy --all --all-namespaces
+krm meshpolicy --all --all-namespaces
+krm PeerAuthentication --all --all-namespaces
+# TODO: some forcing?
+krmns istio-system
