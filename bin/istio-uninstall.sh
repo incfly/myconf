681df285139806c5cb7537b2481e6d3699c747a9
Jianfei Hu <jianfeih@google.com>
2020-05-26 20:20:05 +0000

    remove webhook

--- bin/istio-uninstall.sh
+++ bin/istio-uninstall.sh
@@ -4,5 +4,7 @@ krm dr --all --all-namespaces
 krm policy --all --all-namespaces        
 krm meshpolicy --all --all-namespaces
 krm PeerAuthentication --all --all-namespaces
+# Sidecar injector if still persists can blocks the pod deployment when istiod pod is gone.
+krm MutatingWebhookConfiguration istio-sidecar-injector
 # TODO: some forcing?
 krm ns istio-system
