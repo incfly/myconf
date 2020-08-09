3620b9d2356f5bcd5f330cb31e2a9ea016d80888
Jianfei Hu <jianfeih@google.com>
2020-08-09 20:46:53 +0000

    add devdir alias for istio

--- zsh/istio.sh
+++ zsh/istio.sh
@@ -1,1 +1,8 @@
 alias istioctl=$HOME/workspace/go/src/istio.io/istio/out/linux_amd64/istioctl
+
+# Dev config file root path.
+devc() {
+  cc=${CASE:-"default"}
+  echo "$HOME/workspace/github.com/incfly/sysamples/istio/cases/$cc"
+}
+
