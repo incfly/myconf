d0a3e0f9ff817a780658bc21628c1debf25f1729
Jianfei Hu <jianfeih@google.com>
2020-08-27 22:04:30 -0700

    pc name in PROMPT

--- zsh/.zshrc-lib.sh
+++ zsh/.zshrc-lib.sh
@@ -79,3 +79,4 @@ alias python=python3
 alias watch='watch '
 alias wt='watch '
 alias ta='tmux attach'
+export PROMPT='%{$fg[green]%}%m %{$fg_bold[red]%}➜%{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'
